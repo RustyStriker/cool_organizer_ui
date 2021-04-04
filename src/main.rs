dcc136b3e34c1e06faf32e51b48327f0a5671e34
RustyStriker <avivr903@gmail.com>
2021-04-04 12:53:36 +0300

    first commit stop yelling

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+use gtk::{WindowType, prelude::*};
+
+fn main() {
+    gtk::init().expect("couldnt initialize gtk!");
+
+    let mut window : gtk::Window = gtk::Window::new(WindowType::Toplevel);
+
+    
+}
